9f6570102fdc5d1e14fec2607805115eb4f2f861
Daniil Melnik <daniilmelnik@inbox.ru>
2021-06-07 21:46:00 +0300

    feat: add answers to 1 and 2 tasks

--- Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
@@ -3,6 +3,34 @@ import Foundation
 public extension Int {
     
     var roman: String? {
-        nil
+        
+        if (self <= 0 || self > 3999){
+            return nil
+        }
+        
+        let comparisonArray: [(number: Int, string: String)] =
+        [(1000, "M"),
+        (900, "CM"),
+        (500, "D"),
+        (400, "CD"),
+        (100, "C"),
+        (90, "XC"),
+        (50, "L"),
+        (40, "XL"),
+        (10, "X"),
+        (9, "IX"),
+        (5, "V"),
+        (4, "IV"),
+        (1, "I")]
+        
+        var returnableText = ""
+        var remainder = 0
+        
+        for entry in comparisonArray {
+            let quotient = (self - remainder) / entry.number
+            remainder = remainder + (quotient * entry.number)
+            returnableText += String(repeating: entry.string, count: quotient)
+        }
+        return returnableText
     }
 }
--- Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
@@ -1,8 +1,37 @@
 import Foundation
 
 final class FillWithColor {
+    var intForComparison = 0
+    var copiedArray = [[Int]]()
     
     func fillWithColor(_ image: [[Int]], _ row: Int, _ column: Int, _ newColor: Int) -> [[Int]] {
-        [[]]
+        if (image.first == nil) {
+                    return image
+        }
+        
+        let r = image.count
+        let c = image[0].count
+        if (image.first == nil || row < 0 || row >= r || column < 0 || column >= c) {
+            return image
+        }
+        copiedArray = image
+        intForComparison = image[row][column]
+        
+        leftRightUpDown(row, column, newColor)
+        
+        return copiedArray
+    }
+    
+    private func leftRightUpDown(_ i: Int, _ j: Int, _ newColor: Int){
+        if (i >= 0 && i < copiedArray.count && j >= 0 && j < copiedArray[i].count && copiedArray[i][j] == intForComparison && copiedArray[i][j] != newColor) {
+        copiedArray[i][j] = newColor
+            
+        leftRightUpDown(i - 1, j, newColor)
+        leftRightUpDown(i + 1, j, newColor)
+        leftRightUpDown(i, j + 1, newColor)
+        leftRightUpDown(i, j - 1, newColor)
+        } else {
+        return
+        }
     }
 }
